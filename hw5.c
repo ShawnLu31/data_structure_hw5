f4275977686b01211de55400cf1574aab7cff8f2
ShawnLu31 <john2001131@gmail.com>
2020-11-15 21:39:44 +0800

    modify hw5 for number 0 be hashed

--- hw5.c
+++ hw5.c
@@ -10,8 +10,12 @@ void Print(int table[]);
 int main(){
 	//table bucket
 	int *hash_table = (int*)malloc(TABLE_SIZE * sizeof(int));
+	int *check_list = (int*)malloc(TABLE_SIZE * sizeof(int));//0 is empty, 1 is full
 	for(int i = 0; i < TABLE_SIZE; i++)
+	{
 		*(hash_table + i) = 0;
+		*(check_list + i) = 0;
+	}
 	//input key
 	int key = 0, index = 0;
 	while(scanf("%d", &key) != EOF)
@@ -19,8 +23,9 @@ int main(){
 		for(int i = 0; i < TABLE_SIZE; i++)
 		{
 			index = (hash1(key) + i * hash2(key)) % TABLE_SIZE;
-			if(*(hash_table + index) == 0){
+			if(*(check_list + index) == 0){
 				*(hash_table + index) = key;
+				*(check_list + index) = 1;
 				break;
 			}
 		}
