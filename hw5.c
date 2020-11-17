ee2de5f32d70bdda97d7a3c96491971201932f9a
ShawnLu31 <john2001131@gmail.com>
2020-11-17 15:21:37 +0800

    delete check_list

--- hw5.c
+++ hw5.c
@@ -10,11 +10,9 @@ void Print(int *table);
 int main(){
 	//table bucket
 	int *hash_table = (int*)malloc(TABLE_SIZE * sizeof(int));
-	int *check_list = (int*)malloc(TABLE_SIZE * sizeof(int));//0 is empty, 1 is full
 	for(int i = 0; i < TABLE_SIZE; i++)
 	{
 		*(hash_table + i) = 0;
-		*(check_list + i) = 0;
 	}
 	//input key
 	int key = 0, index = 0;
@@ -23,16 +21,14 @@ int main(){
 		for(int i = 0; i < TABLE_SIZE; i++)
 		{
 			index = (hash1(key) + i * hash2(key)) % TABLE_SIZE;
-			if(*(check_list + index) == 0){
+			if(*(hash_table + index) == 0){
 				*(hash_table + index) = key;
-				*(check_list + index) = 1;
 				break;
 			}
 		}
 	}
 	Print(hash_table);
 	free(hash_table);
-	free(check_list);
 	return 0;
 }
 
